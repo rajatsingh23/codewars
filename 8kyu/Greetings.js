ded6110b4f8824290573cf0588735f792c8d8490
Rajat Singh <rs2382001@gmail.com>
2024-12-01 11:03:56 +0530

    Greetings

---
+++ 8kyu/Greetings.js
@@ -0,0 +1,16 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+//solution
+
+function greet (name, owner) {
+    // Add code here
+    if(name === owner){
+      return "Hello boss"
+    }return "Hello guest"
+  }
