dec05984fccd456b9473b749619dec84b69630c8
Rajat Singh <rs2382001@gmail.com>
2025-04-04 12:13:13 +0530

    Not all but sometimes all

---
+++ 7kyu/notAllButSometimesAll.js
@@ -0,0 +1,27 @@
+// Write
+
+// function remove(str, what)
+// that takes in a string str(text in Python) and an object/hash/dict/Dictionary what and returns a string with the chars removed in what. For example:
+
+// remove('this is a string',{'t':1, 'i':2}) === 'hs s a string'
+// // remove from 'this is a string' the first 1 't' and the first 2 i's.
+// remove('hello world',{'x':5, 'i':2}) === 'hello world'
+// // there are no x's or i's, so nothing gets removed
+// remove('apples and bananas',{'a':50, 'n':1}) === 'pples d bnns'
+// // we don't have 50 a's, so just remove it till we hit end of string.
+
+//solution
+
+function remove(str, what) {
+    let arr = Object.keys(what)
+    let intarr = Object.values(what)
+    let newStr = str
+    for(let i = 0; i<arr.length; i++){
+      for(let j = 0; j<intarr[i]; j++){
+        console.log(arr[i])
+        newStr = newStr.replace(new RegExp(arr[i]), '')
+      }
+    }
+    return newStr;
+  }
+  
